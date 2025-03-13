313e6cb444d82acefe564c07dc4491ff5aaa78b0
skye-fox <105951568+skye-fox@users.noreply.github.com>
2025-03-13 07:02:25 -0500

    Animate the backbuffer

--- src/win32_handmade.zig
+++ src/win32_handmade.zig
@@ -15,62 +15,103 @@ const zig32 = struct {
     usingnamespace @import("win32").zig;
 };
 
-var bitmap_memory: ?*anyopaque = undefined;
-var bitmap_info: zig32.BITMAPINFO = std.zeroInit(zig32.BITMAPINFO, .{});
-var bitmap_width: i32 = 0;
-var bitmap_height: i32 = 0;
+const Color = packed struct(u32) {
+    blue: u8,
+    green: u8,
+    red: u8,
+    _: u8 = 0,
+};
+
+const Win32OffscreenBuffer = struct {
+    // NOTE: Pixels are always 32-bits wide, memory order BB GG RR XX
+    info: zig32.BITMAPINFO,
+    memory: ?*anyopaque,
+    width: i32,
+    height: i32,
+    pitch: u32,
+};
+
+const win32WindowDimension = struct {
+    width: i32,
+    height: i32,
+};
 
 var global_running = false;
+var global_back_buffer: Win32OffscreenBuffer = std.zeroInit(Win32OffscreenBuffer, .{});
 
-fn win32ResizeDIBSection(width: i32, height: i32) void {
-    if (bitmap_memory != null) {
-        _ = zig32.VirtualFree(bitmap_memory, 0, zig32.MEM_RELEASE);
-    }
+fn win32GetWindowDimension(window: zig32.HWND) win32WindowDimension {
+    var result: win32WindowDimension = std.zeroInit(win32WindowDimension, .{});
 
-    bitmap_width = width;
-    bitmap_height = height;
+    var client_rect: zig32.RECT = std.zeroInit(zig32.RECT, .{});
+    _ = zig32.GetClientRect(window, &client_rect);
+    result.width = client_rect.right - client_rect.left;
+    result.height = client_rect.bottom - client_rect.top;
 
-    bitmap_info.bmiHeader.biSize = @sizeOf(@TypeOf(bitmap_info.bmiHeader));
-    bitmap_info.bmiHeader.biWidth = width;
-    bitmap_info.bmiHeader.biHeight = -height;
-    bitmap_info.bmiHeader.biPlanes = 1;
-    bitmap_info.bmiHeader.biBitCount = 32;
-    bitmap_info.bmiHeader.biCompression = zig32.BI_RGB;
-
-    const bytes_per_pixel: u8 = 4;
-    const bitmap_memory_size: usize = @intCast((width * height) * bytes_per_pixel);
-    bitmap_memory = zig32.VirtualAlloc(null, bitmap_memory_size, zig32.MEM_COMMIT, zig32.PAGE_READWRITE);
+    return result;
 }
 
-fn win32UpdateWindow(device_context: ?zig32.HDC, window_rect: *zig32.RECT) void {
-    const window_width: i32 = window_rect.right - window_rect.left;
-    const window_height: i32 = window_rect.bottom - window_rect.top;
-    _ = zig32.StretchDIBits(device_context, 0, 0, bitmap_width, bitmap_height, 0, 0, window_width, window_height, bitmap_memory, &bitmap_info, zig32.DIB_RGB_COLORS, zig32.SRCCOPY);
+fn win32RenderWeirdGradient(buffer: Win32OffscreenBuffer, blue_offset: i32, green_offset: i32) void {
+    var row: [*]u8 = @ptrCast(buffer.memory);
+    var y: i32 = 0;
+    while (y < buffer.height) : (y += 1) {
+        var x: i32 = 0;
+
+        // NOTE: pixel in memory: BB GG RR xx
+        var pixel: [*]Color = @alignCast(@ptrCast(row));
+        while (x < buffer.width) : (x += 1) {
+            const blue: u8 = @truncate(@abs(x + blue_offset));
+            const green: u8 = @truncate(@abs(y + green_offset));
+            // NOTE: bitwise method (This is how Casey Muratori did it)
+            // pixel[0] = ((@as(u32, green) << 8) | blue);
+            // pixel += 1;
+
+            // NOTE: packed struct method (not possible in C, but perhaps the preferred method in zig?)
+            pixel[0] = .{ .blue = blue, .green = green, .red = 0 };
+            pixel += 1;
+        }
+        row += buffer.pitch;
+    }
+}
+
+fn win32ResizeDIBSection(buffer: *Win32OffscreenBuffer, width: i32, height: i32) void {
+    if (buffer.memory != null) {
+        _ = zig32.VirtualFree(buffer.memory, 0, zig32.MEM_RELEASE);
+    }
+
+    buffer.width = width;
+    buffer.height = height;
+    const bytes_per_pixel: u8 = 4;
+
+    buffer.info.bmiHeader.biSize = @sizeOf(@TypeOf(buffer.info.bmiHeader));
+    buffer.info.bmiHeader.biWidth = width;
+    buffer.info.bmiHeader.biHeight = -height;
+    buffer.info.bmiHeader.biPlanes = 1;
+    buffer.info.bmiHeader.biBitCount = 32;
+    buffer.info.bmiHeader.biCompression = zig32.BI_RGB;
+
+    const bitmap_memory_size: usize = @intCast((buffer.width * buffer.height) * bytes_per_pixel);
+    buffer.memory = zig32.VirtualAlloc(null, bitmap_memory_size, zig32.MEM_COMMIT, zig32.PAGE_READWRITE);
+
+    buffer.pitch = @intCast(width * bytes_per_pixel);
+}
+
+fn win32DisplayBufferInWindow(device_context: ?zig32.HDC, width: i32, height: i32, buffer: Win32OffscreenBuffer) void {
+    _ = zig32.StretchDIBits(device_context, 0, 0, width, height, 0, 0, buffer.width, buffer.height, buffer.memory, &buffer.info, zig32.DIB_RGB_COLORS, zig32.SRCCOPY);
 }
 
 fn win32MainWindowCallBack(window: zig32.HWND, message: u32, w_param: zig32.WPARAM, l_param: zig32.LPARAM) callconv(.c) zig32.LRESULT {
     var result: zig32.LRESULT = 0;
     switch (message) {
-        zig32.WM_CLOSE, zig32.WM_DESTROY => {
-            global_running = false;
-        },
+        zig32.WM_CLOSE, zig32.WM_DESTROY => global_running = false,
         zig32.WM_ACTIVATEAPP => {},
         zig32.WM_PAINT => {
             var paint: zig32.PAINTSTRUCT = std.zeroInit(zig32.PAINTSTRUCT, .{});
             const device_context: ?zig32.HDC = zig32.BeginPaint(window, &paint);
 
-            var client_rect: zig32.RECT = std.zeroInit(zig32.RECT, .{});
-            _ = zig32.GetClientRect(window, &client_rect);
+            const dimension: win32WindowDimension = win32GetWindowDimension(window);
 
-            win32UpdateWindow(device_context, &client_rect);
+            win32DisplayBufferInWindow(device_context, dimension.width, dimension.height, global_back_buffer);
             _ = zig32.EndPaint(window, &paint);
-        },
-        zig32.WM_SIZE => {
-            var client_rect: zig32.RECT = std.zeroInit(zig32.RECT, .{});
-            _ = zig32.GetClientRect(window, &client_rect);
-            const width: i32 = client_rect.right - client_rect.left;
-            const height: i32 = client_rect.bottom - client_rect.top;
-            win32ResizeDIBSection(width, height);
         },
         else => {
             result = zig32.DefWindowProcA(window, message, w_param, l_param);
@@ -85,6 +126,9 @@ pub fn wWinMain(instance: zig32.HINSTANCE, prev_instance: ?zig32.HINSTANCE, cmd_
     _ = cmd_show;
 
     var window_class = std.zeroInit(zig32.WNDCLASSA, .{});
+
+    win32ResizeDIBSection(&global_back_buffer, 1280, 720);
+
     window_class.style = .{ .HREDRAW = 1, .VREDRAW = 1, .OWNDC = 1 };
     window_class.lpfnWndProc = win32MainWindowCallBack;
     window_class.hInstance = instance;
@@ -106,16 +150,27 @@ pub fn wWinMain(instance: zig32.HINSTANCE, prev_instance: ?zig32.HINSTANCE, cmd_
             null,
         );
         if (window) |_| {
-            var message: zig32.MSG = undefined;
+            const device_context: ?zig32.HDC = zig32.GetDC(window);
+
+            var x_offset: i32 = 0;
+            var y_offset: i32 = 0;
+
             global_running = true;
             while (global_running) {
-                const message_result = zig32.GetMessageA(&message, null, 0, 0);
-                if (message_result > 0) {
+                var message: zig32.MSG = undefined;
+                while (zig32.PeekMessageA(&message, null, 0, 0, zig32.PM_REMOVE) != 0) {
+                    if (message.message == zig32.WM_QUIT) global_running = false;
                     _ = zig32.TranslateMessage(&message);
                     _ = zig32.DispatchMessageA(&message);
-                } else {
-                    break;
                 }
+
+                win32RenderWeirdGradient(global_back_buffer, x_offset, y_offset);
+
+                const dimension: win32WindowDimension = win32GetWindowDimension(window.?);
+                win32DisplayBufferInWindow(device_context, dimension.width, dimension.height, global_back_buffer);
+
+                x_offset += 1;
+                y_offset += 2;
             }
         } else {
             // TODO: Logging
